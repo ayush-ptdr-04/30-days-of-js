fe49671c99976117d378a94ad2727dcd5e3fa775
aayush patidar <ap6887980@gmail.com>
2025-03-09 12:29:10 +0530

    this is day 11

--- Day10.js
+++ Day10.js
@@ -1,5 +1,5 @@
-var once = function (fn) {
-  called = false;
+var once = (fn) => {
+  let called = false;
   return function (...args) {
     if (called) {
       return undefined;
@@ -8,3 +8,8 @@ var once = function (fn) {
     return fn(...args);
   };
 };
+
+let fn = (a, b, c) => a + b + c;
+let onceFn = once(fn);
+onceFn(1, 2, 3);
+onceFn(1, 3, 3);
