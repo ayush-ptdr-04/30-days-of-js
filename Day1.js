1a3d368b7f06da5ac91ee7843b826132bc37ba53
aayush patidar <ap6887980@gmail.com>
2025-05-10 23:21:03 +0530

    solve new qsns abc companies

--- Day1.js
+++ Day1.js
@@ -1,6 +1,15 @@
-function createHelloWorld() {
-  function f() {
-    return "Hello World";
+let n = 2;
+let arr = [2, 4, 5, 5, 6, 5, 3];
+
+function chunkArray(arr, n) {
+  let result = [];
+  if (n > 0) {
+    for (let i = 0; i < arr.length; i += n) {
+      result.push(arr.slice(i, i + n));
+    }
+    return result;
   }
-  return f;
 }
+
+console.log(chunkArray(arr, n));
+// module.exports = chunkArray;
